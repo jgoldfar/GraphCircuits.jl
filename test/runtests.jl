cd0515a748d0bebcaad82cd89f5ecbff01a132b7
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-10-16 07:30:17 -0400

    Start beginnings of a test script, add LightGraphs to REQUIRE file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using GraphCircuits
 using Base.Test
-
+using LightGraphs
 # write your own tests here
-@test 1 == 2
+@testset begin
+    k = 10
+    tarjan_worst_case = DiGraph(k)
+    for i in 1:(k-1)
+        add_edge!(tarjan_worst_case, i, i + 1)
+    end
+    add_edge!(tarjan_worst_case, k, 1)
+    find_circuits(tarjan_worst_case)
+end
