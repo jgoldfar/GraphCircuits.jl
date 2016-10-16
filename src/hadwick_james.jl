8cf44013f4a17ed6acd64637ea21bb1a61e6830d
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-10-16 18:52:56 -0400

    Start Hadwick and James algorithm implementation in a more idiomatic "Julia" way. Will profile after implementation is completed.

--- src/hadwick_james.jl
+++ src/hadwick_james.jl
@@ -1,3 +1,41 @@
 find_circuits(g::DiGraph, ::Type{Val{:hadwick_james}}) = hadwick_james(g)
 
-function hadwick_james(g::DiGraph) end
+function hadwick_james(g::DiGraph)
+B = Vector{Vector{Int}}()
+blocked = Vector{Bool}()
+lengthHisto = Vector{Culong}()
+vertexPopularity = Vector{Vector{Culong}}()
+cycles = Vector{Vector{Int}}()
+
+return cycles
+end
+
+countAkArcs(g) = ne(g) # Number of edges (Arcs) in graph
+
+function countAndFilter!{T}(val::T, list::AbstractArray{T})
+    nocc = 0
+    function doFilter(v)
+        if v == val
+            nocc += 1
+            false
+        else
+            true
+        end
+    end
+    filter!(doFilter, list)
+    return nocc
+end
+
+function unblock!{T}(v::T, blocked::Vector{Bool}, B::Vector{Vector{T}})
+    blocked[v] = false
+    wPos = 1
+    Bv = B[v]
+    while wPos <= length(Bv)
+        w = Bv[wPos]
+        wPos += 1- countAndFilter!(w, Bv)
+        if blocked(w)
+            unblock!(w, blocked, B)
+        end
+    end
+    return nothing
+end
