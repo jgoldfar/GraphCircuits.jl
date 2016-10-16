9904dce27dbdd91fa6219af76eb0461fa8b65dff
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-10-16 07:59:08 -0400

    Add preliminary implementation outline for Hadwick & James and Johnson algorithms (with dispatch choosing Hadwick & James by default)

--- src/GraphCircuits.jl
+++ src/GraphCircuits.jl
@@ -1,5 +1,10 @@
 module GraphCircuits
+using LightGraphs
 
-# package code goes here
+include("johnson.jl")
+
+include("hadwick_james.jl")
+
+findcycles(g::DiGraph) = findcycles(g, Val{:johnson})
 
 end # module
---
+++ src/hadwick_james.jl
@@ -0,0 +1,3 @@
+findcycles(g::DiGraph, ::Type{Val{:hadwick_james}}) = hadwick_james(g)
+
+function hadwick_james(g::DiGraph) end
