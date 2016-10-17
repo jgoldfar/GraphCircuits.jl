9c7fec2fd03da56925e1b78a17634b3ebc4e0dea
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-10-17 16:05:35 -0400

    Remove comparison between johnson (unimplemented) and Hadwick & James algorithm, and test sets of cycles for equality using the definition of set equality rather than Julia's equality, which is too strict (for us, order doesn't matter.)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,35 @@ using GraphCircuits
 using Base.Test
 using LightGraphs
 
-# write your own tests here
 @testset begin
-    k = 10
-    tarjan_worst_case = DiGraph(k)
-    for i in 1:(k-1)
-        add_edge!(tarjan_worst_case, i, i + 1)
-    end
-    add_edge!(tarjan_worst_case, k, 1)
-    johnson_circuits = find_circuits(tarjan_worst_case, Val{:johnson})
+    # k = 10
+    # tarjan_worst_case = DiGraph(k)
+    # Note: This is a CycleDiGraph(k)
+    # for i in 1:(k-1)
+    #     add_edge!(tarjan_worst_case, i, i + 1)
+    # end
+    # add_edge!(tarjan_worst_case, k, 1)
+    # johnson_circuits = find_circuits(tarjan_worst_case, Val{:johnson})
+    #
+    # hadwick_circuits = find_circuits(tarjan_worst_case, Val{:hadwick_james})
+    #
+    # @test johnson_circuits == hadwick_circuits
 
-    hadwick_circuits = find_circuits(tarjan_worst_case, Val{:hadwick_james})
+    hadwick_james_ex1 = DiGraph(5)
+    add_edge!(hadwick_james_ex1, 1, 2)
+    add_edge!(hadwick_james_ex1, 2, 3)
+    add_edge!(hadwick_james_ex1, 3, 4)
+    add_edge!(hadwick_james_ex1, 3, 5)
+    add_edge!(hadwick_james_ex1, 4, 5)
+    add_edge!(hadwick_james_ex1, 5, 2)
 
-    @test johnson_circuits == hadwick_circuits
+    johnson_circuits = find_circuits(hadwick_james_ex1, Val{:johnson})
+    hadwick_circuits = find_circuits(hadwick_james_ex1, Val{:hadwick_james})
+
+    expected_circuits = Vector{Int}[
+        [2, 3, 4, 5],
+        [2, 3, 5]
+    ]
+    @test issubset(expected_circuits, hadwick_circuits)
+    @test issubset(hadwick_circuits, expected_circuits)
 end
