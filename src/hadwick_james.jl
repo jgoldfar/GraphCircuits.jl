18a27b1b30796e527d7bc9d0d4bbe6d207e5bf2e
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-04-24 16:46:51 -0400

    Generalize eltype of the DiGraph and incorporate better tests subsequent to LightGraphs PR: https://github.com/JuliaGraphs/LightGraphs.jl/pull/600

--- src/hadwick_james.jl
+++ src/hadwick_james.jl
@@ -8,10 +8,11 @@ Find circuits using circuit! for each vertex in the graph `g`.
 """
 function hadwick_james(g::DiGraph)
     nvg = nv(g)
-    B = [Int[] for i in 1:nvg]
+    T = eltype(g)
+    B = [T[] for i in 1:nvg]
     blocked = [false for i in 1:nvg]
-    stack = Int[]
-    cycles = Vector{Vector{Int}}()
+    stack = T[]
+    cycles = Vector{Vector{T}}()
     for v in vertices(g)
         circuit!(g, v, v, blocked, B, stack, cycles)
         resetblocked!(blocked)
@@ -23,7 +24,7 @@ end
 """
 Reset B work structure.
 """
-resetB!(B) = map!(empty!, B)
+resetB!(B) = map!(empty!, B, B)
 
 """
 Reset vector of "blocked" vertices.
@@ -33,31 +34,27 @@ resetblocked!(blocked) = fill!(blocked, false)
 """
 Find circuits starting from v1 recursively.
 """
-function circuit!(g::DiGraph, v1::Int, v2::Int, blocked::Vector{Bool}, B::Vector{Vector{Int}}, stack::Vector{Int}, cycles::Vector{Vector{Int}})
+function circuit!(g::DiGraph, v1::T, v2::T, blocked::Vector{Bool}, B::Vector{Vector{T}}, stack::Vector{T}, cycles::Vector{Vector{T}}) where T
     f = false
     push!(stack, v2)
     blocked[v2] = true
 
-    Av = map(dst, out_edges(g, v2))
+    Av = out_neighbors(g, v2)
     for w in Av
         (w < v1) && continue
         if w == v1 # Found a circuit
             push!(cycles, copy(stack))
             f = true
         elseif !blocked[w]
-            if circuit!(g, v1, w, blocked, B, stack, cycles)
-                f = true
-            end
+            f = circuit!(g, v1, w, blocked, B, stack, cycles)
         end
     end
     if f
         unblock!(v2, blocked, B)
     else
         for w in Av
-            if w < v1
-                continue
-            end
-            if !in(v2, B[w])
+            (w < v1) && continue
+            if !(v2 in B[w])
                 push!(B[w], v2)
             end
         end
@@ -69,7 +66,7 @@ end
 """
 Simultaneously count and remove occurences of a value `val` in the array `list`.
 """
-function countAndFilter!{T}(val::T, list::AbstractArray{T})
+function countAndFilter!(val::T, list::AbstractArray{T}) where T
     nocc = 0
     function doFilter(v)
         if v == val
@@ -86,16 +83,14 @@ end
 """
 Unblock the value `v` from the blocked list and remove from `B`.
 """
-function unblock!{T}(v::T, blocked::Vector{Bool}, B::Vector{Vector{T}})
+function unblock!(v::T, blocked::Vector{Bool}, B::Vector{Vector{T}}) where T
     blocked[v] = false
     wPos = 1
     Bv = B[v]
     while wPos <= length(Bv)
         w = Bv[wPos]
         wPos += 1 - countAndFilter!(w, Bv)
-        if blocked[w]
-            unblock!(w, blocked, B)
-        end
+        blocked[w] && unblock!(w, blocked, B)
     end
     return nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,20 +7,7 @@ else
 end # Use next-gen test framework for all Julia versions.
 using LightGraphs
 
-@testset begin
-    # k = 10
-    # tarjan_worst_case = DiGraph(k)
-    # Note: This is a CycleDiGraph(k)
-    # for i in 1:(k-1)
-    #     add_edge!(tarjan_worst_case, i, i + 1)
-    # end
-    # add_edge!(tarjan_worst_case, k, 1)
-    # johnson_circuits = find_circuits(tarjan_worst_case, Val{:johnson})
-    #
-    # hadwick_circuits = find_circuits(tarjan_worst_case, Val{:hadwick_james})
-    #
-    # @test johnson_circuits == hadwick_circuits
-
+@testset "Cycles" begin
     hadwick_james_ex1 = DiGraph(5)
     add_edge!(hadwick_james_ex1, 1, 2)
     add_edge!(hadwick_james_ex1, 2, 3)
@@ -29,7 +16,7 @@ using LightGraphs
     add_edge!(hadwick_james_ex1, 4, 5)
     add_edge!(hadwick_james_ex1, 5, 2)
 
-    johnson_circuits = find_circuits(hadwick_james_ex1, Val{:johnson})
+    # johnson_circuits = find_circuits(hadwick_james_ex1, Val{:johnson})
     hadwick_circuits = find_circuits(hadwick_james_ex1, Val{:hadwick_james})
 
     expected_circuits = Vector{Int}[
@@ -38,4 +25,30 @@ using LightGraphs
     ]
     @test issubset(expected_circuits, hadwick_circuits)
     @test issubset(hadwick_circuits, expected_circuits)
+
+    add_edge!(hadwick_james_ex1, 1, 1)
+    add_edge!(hadwick_james_ex1, 3, 3)
+
+    hadwick_circuits_self = find_circuits(hadwick_james_ex1, Val{:hadwick_james})
+
+    @test issubset(expected_circuits, hadwick_circuits_self)
+    @test [1] ∈ hadwick_circuits_self && [3] ∈ hadwick_circuits_self
+
+    ex2_size = 10
+    hadwick_james_ex2 = PathDiGraph(ex2_size)
+    @test isempty(find_circuits(hadwick_james_ex2, Val{:hadwick_james}))
+
+    ex3_size = 5
+    hadwick_james_ex3 = CompleteDiGraph(ex3_size)
+    @test length(find_circuits(hadwick_james_ex3, Val{:hadwick_james})) == length(unique(find_circuits(hadwick_james_ex3, Val{:hadwick_james})))
+
+    # Almost fully connected DiGraph
+    ex4 = DiGraph(9)
+    for (src, dest) in [(1,2),(1,5),(1,7),(1,8),(2,9),(3,4),(3,6),
+                        (4,5),(4,7),(5,6),(6,7),(6,8),(7,9),(8,9)]
+        add_edge!(ex4, src, dest)
+        add_edge!(ex4, dest, src)
+    end
+    ex4_output = simplecycles_hadwick_james(ex4)
+    @test [1, 2] ∈ ex4_output && [8, 9] ∈ ex4_output
 end
